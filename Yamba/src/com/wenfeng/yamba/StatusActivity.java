3af3cae65841d912adc6028522730dd374934c87
Elric Gao <elricfeng@gmail.com>
2014-11-09 00:56:53 +0100

    Setup the projet.
    1. Finish a simple UI of StatusActivity
    2. Post to Yamba service by using Yamba API
    3. Change the UI thread by using asyncTask

---
+++ Yamba/src/com/wenfeng/yamba/StatusActivity.java
@@ -0,0 +1,68 @@
+package com.wenfeng.yamba;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+public class StatusActivity extends Activity implements OnClickListener {
+	private static final String TAG = "StatusActivity";
+	private EditText editStatus;
+	private Button buttonTweet;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
+		
+		editStatus = (EditText)findViewById(R.id.editStatus);
+		buttonTweet = (Button)findViewById(R.id.buttonTweet);
+		
+		buttonTweet.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.status, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		String status = editStatus.getContext().toString();
+		Log.d(TAG, "onClicked with status:" + status);
+		new PostTask().execute(status);
+	}
+
+	private final class PostTask extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			YambaClient yambaCloud = new YambaClient("student", "password");
+			try {
+				yambaCloud.postStatus(params[0]);
+				return "Successfully posted";
+			} catch (YambaClientException e) {
+				e.printStackTrace();
+				return "Failed to post to Yamba service";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+		
+	}
+}
